be6a95e6bf3cb5085882cb8205e5cda356bfb166
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-02-20 20:22:58 +0500

    feat: eureka server initial commit

---
+++ eureka-movies/src/test/java/com/microservices/eurekamovies/EurekaMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.eurekamovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
